d06bcf9a3e1499d3493d5fc2cf9179756d3afdf0
João Pedro <146018437+JoaoPedro2701@users.noreply.github.com>
2025-07-04 11:41:13 -0300

    Add files via upload

---
+++ Tabuada/src/code.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class code {
+
+    public static void main(String[] args) {
+
+        // Tabuada
+    	
+        Scanner sc = new Scanner(System.in);
+
+        int cap;
+
+        System.out.print("Digite o número que deseja saber sua tabuada: ");
+        cap = sc.nextInt();
+
+        for (int resu = 1; resu <= 10; resu++) {
+            int nuf = cap * resu;
+            System.out.println(cap + " x " + resu + " = " + nuf);
+        }
+
+        sc.close();
+    }
+}
